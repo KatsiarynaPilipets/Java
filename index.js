610e6f72a60b4bc9d58ac25b6ed11cdded5736ac
kotenochek27 <katrinya1@gmail.com>
2023-05-05 21:20:50 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+let x = 40000;
+let y = 22000;
+
+console.log(x + y);
+
+// Задание 1
+
+let message = 'Владивосток ';
+let b = 2000;
+
+console.log(message+b);
+
+// Задание 2
+
+let c = 128;
+let d = 254;
+let e = 137;
+let f = 201;
+
+console.log((c+d+e+f)/60);
+
+// Задание 3
+
+let g =2;
+
+console.log(g**5);
+
+// Задание 4
+
+let text = 'Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!';
+
+console.log(text);
+
+// Задание 5
+
+const greetings = "Привет";
+const name = "Кот";
+
+console.log(greetings + ', ' + name);
+
+// Задание 6
+
+const time = 34;
+let message2 = 'Старт поездки. Осталось минут: ';
+let time2 = 15;
+
+console.log(message2 + (time - time2));
+
+let message3 = 'Немного сториз в соцсетях. Осталось минут: ';
+let time3 = 10;
+
+console.log(message3 + (time - time2 - time3));
+
+let message4 = 'Вы приехали. Добро пожаловать в Москву';
+
+console.log(message4);
+
+// Задание 7
+
+const fahrenheit = 451;
+const celsius = (fahrenheit - 32) / 1.8;
+
+console.log(fahrenheit + ' градуса по Фаренгейту - это ' + celsius + ' градуса по Цульсиюю');
+
+// Задание 8
